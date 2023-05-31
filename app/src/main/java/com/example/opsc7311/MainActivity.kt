6275b16a2ba7c0195c39a998dc7392b72b6d0ff5
Rial Seebran <ST10083702@vcconnect.edu.za>
2023-06-01 01:07:37 +0200

    Fixed date and time pickers.

--- app/src/main/java/com/example/opsc7311/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.opsc7311
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +28,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.opsc7311.ui.theme.Opsc7311Theme
+import com.maxkeppeker.sheets.core.models.base.UseCaseState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+import com.maxkeppeler.sheets.clock.ClockDialog
+import com.maxkeppeler.sheets.clock.models.ClockSelection
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +57,72 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TimeSheetApp(modifier: Modifier = Modifier) {
 
-    var showDatePicker by remember { mutableStateOf(false) }
-    if (showDatePicker) {
-        // Show it
-    }
+
+    val localDateToDate = SimpleDateFormat(
+        "yyyy-MM-dd",
+        Locale.getDefault()
+    )
+
+    val timeToDate = SimpleDateFormat(
+        "HH:mm",
+        Locale.getDefault()
+    )
+
+    val timeToDateDisplay = SimpleDateFormat(
+        "hh:mm aa",
+        Locale.getDefault()
+    )
+
+    val dateToTextDisplay = SimpleDateFormat(
+        "dd MMMM yyyy",
+        Locale.getDefault()
+    )
+
+    var selectedDate: Date by remember { mutableStateOf(Date()) }
+    val formattedDate = dateToTextDisplay.format(selectedDate)
+
+    var startTime: Date by remember { mutableStateOf(Date()) }
+    val formattedStartTime = timeToDateDisplay.format(startTime)
+
+    var endTime: Date by remember { mutableStateOf(Date()) }
+    val formattedEndTime = timeToDateDisplay.format(endTime)
+
+
+    val calendarState = UseCaseState()
+
+    CalendarDialog(
+        state = calendarState,
+        config = CalendarConfig(
+            monthSelection = true,
+            yearSelection = true
+        ),
+        selection = CalendarSelection.Date { date ->
+            selectedDate = localDateToDate.parse(date.toString()) as Date
+        }
+    )
+
+    val clockState1 = UseCaseState()
+
+    ClockDialog(
+        state = clockState1,
+        selection = ClockSelection.HoursMinutes { hours, minutes ->
+            startTime = timeToDate.parse("${hours}:${minutes}") as Date
+
+        }
+    )
+
+    val clockState2 = UseCaseState()
+
+    ClockDialog(
+        state = clockState2,
+        selection = ClockSelection.HoursMinutes { hours, minutes ->
+            endTime = timeToDate.parse("${hours}:${minutes}") as Date
+        }
+    )
 
     Column(
         modifier = Modifier.padding(all = 16.dp)
@@ -60,21 +131,30 @@ fun TimeSheetApp(modifier: Modifier = Modifier) {
             modifier = modifier
                 .fillMaxWidth()
                 .padding(8.dp),
+            date = formattedDate,
             onIconClicked = {
-                showDatePicker = true
+                calendarState.show()
             }
         )
         TimeSurface(
             title = "Start Time",
             modifier = modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .padding(8.dp),
+            time = formattedStartTime,
+            onIconClicked = {
+                clockState1.show()
+            }
         )
         TimeSurface(
             title = "End Time",
             modifier = modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .padding(8.dp),
+            time = formattedEndTime,
+            onIconClicked = {
+                clockState2.show()
+            }
         )
     }
 
@@ -82,6 +162,7 @@ fun TimeSheetApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun DateSurface(
+    date: String,
     onIconClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -98,7 +179,7 @@ fun DateSurface(
                     style = MaterialTheme.typography.labelLarge
                 )
                 Text(
-                    text = "11 May 2023",
+                    text = date,
                     style = MaterialTheme.typography.headlineMedium
                 )
             }
@@ -113,7 +194,12 @@ fun DateSurface(
 }
 
 @Composable
-fun TimeSurface(title: String, modifier: Modifier = Modifier) {
+fun TimeSurface(
+    title: String,
+    time: String,
+    onIconClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Surface(
         modifier = modifier
     ) {
@@ -127,11 +213,11 @@ fun TimeSurface(title: String, modifier: Modifier = Modifier) {
                     style = MaterialTheme.typography.labelLarge
                 )
                 Text(
-                    text = "7:43AM",
+                    text = time,
                     style = MaterialTheme.typography.headlineMedium
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onIconClicked) {
                 Icon(
                     imageVector = Icons.Default.Edit,
                     contentDescription = "Edit"
